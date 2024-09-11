1e231ec9b70a08d897b547735bbd3673abef1e40
iFlintksks <rafaelnuneszeiro2@gmail.com>
2024-09-11 10:10:49 -0400

    Alinhamneto centralizado do texto footer

--- App/Views/footer.php
+++ App/Views/footer.php
@@ -1,6 +1,6 @@
 <footer class="bg-dark p-5 text-light">
-    <div class="container">
-        <small>
+    <div class="container text-center">
+        <small>''
             Aula de PHP Orientado a Objetos e MVC. Versão <?= APP_VERSAO?>
             <div class="border-top mt-3">
                 &COPY; 2024 - <?= date('Y') ?> IFRO - Guajará-Mirim / Informática - Programação Web
