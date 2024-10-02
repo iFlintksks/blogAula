d7e521f0f74cc0f0c47bb1b0415307169f26f45f
iFlintksks <rafaelnuneszeiro2@gmail.com>
2024-10-02 10:12:23 -0400

    Database

--- App/Libraries/Database.php
+++ App/Libraries/Database.php
@@ -6,6 +6,7 @@ class Database{
     private $banco = 'blogaula';
     private $porta = '3306';
     private $dbh;
+    private $stmt;
     
     public function __construct(){
 
@@ -21,5 +22,43 @@ class Database{
             die();
         }
         
+    }
+    public function query($sql){
+        $this->stmt = $this->dbh->prepare($sql);
+    }
+    public function bind($parametro, $valor, $tipo= null){
+        if(is_null($tipo)):
+            switch(true):
+                case is_int($valor):
+                    $tipo = PDO::PARAM_INT;
+                    break;
+                case is_bool($valor):
+                    $tipo = PDO::PARAM_BOOL;
+                    break;
+                case is_null($valor):
+                    $tipo = PDO::PARAM_NULL;
+                     break;
+                default:
+                    $tipo = PDO::PARAM_STR;
+            endswitch;
+        endif;
+        $this->stmt->bindValue($parametro, $valor, $tipo);
+    }
+    public function executa(){
+        return $this->stmt->execute();
+    }
+    public function resultado(){
+        $this->executa();
+        return $this->stmt->fetch(PDO::FETCH_OBJ);
+    }
+    public function resultados(){
+        $this->executa();
+        return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+    public function totalResultado(){
+        return $this->stmt->rowCount();
+    }
+    public function ultimoIdInserido(){
+        return $this->dbh->lastInsertId();
     }
 }
